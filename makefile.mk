56dfb68f05b9028c1cc04e77edc9f73554758031
Nyx <nyxmalware@gmail.com>
2026-06-03 21:27:35 +0300

    Update makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -4,12 +4,12 @@ LD = ld
 QEMU = qemu-system-x86_64
 IMG = solaros.img
 
-ASM_OBJS = boot.o initrix.o kernel.o
+ASM_OBJS = initrix.o kernel.o
 
 all: build_asm build_rust link build_img
 
 build_asm:
-	$(NASM) -f elf32 boot.asm -o boot.o
+	$(NASM) -f bin boot.asm -o boot.bin
 	$(NASM) -f elf32 initrix.asm -o initrix.o
 	$(NASM) -f elf32 kernel.asm -o kernel.o
 
@@ -17,13 +17,13 @@ build_rust:
 	cd rust && $(RUSTC) build --release
 
 link:
-	$(LD) -m elf_i386 -T link.ld -o solaros.bin $(ASM_OBJS) rust/target/release/libsolaros_rust.a
+	$(LD) -m elf_i386 -T link.ld -o solaros.bin initrix.o kernel.o rust/target/release/libsolaros_rust.a
 
 build_img:
-	objcopy -O binary solaros.bin $(IMG)
+	cat boot.bin solaros.bin > $(IMG)
 
 clean:
-	rm -f *.o *.bin *.img
+	rm -f boot.bin *.o *.bin *.img
 	cd rust && $(RUSTC) clean
 
 run:
