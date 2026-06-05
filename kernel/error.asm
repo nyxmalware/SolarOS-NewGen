d149f145982993f57b21874a707f568cefb3c21c
Nyx <nyxmalware@gmail.com>
2026-06-05 21:17:47 +0300

    Rename print_string to print_error and update calls

--- kernel/error.asm
+++ kernel/error.asm
@@ -1,20 +1,20 @@
-; kernel/error.asm - обработчик ошибок
+; kernel/error.asm - обработчик ошибок для disk/read.asm и disk/fat12.asm
 global error_handler
 
 error_handler:
     pusha
-    call print_string
+    call print_error
     mov ah, 0
     int 0x16
     popa
     jmp 0xFFFF:0
 
-print_string:
+print_error:
     lodsb
     or al, al
     jz .done
     mov ah, 0x0E
     int 0x10
-    jmp print_string
+    jmp print_error
 .done:
     ret
