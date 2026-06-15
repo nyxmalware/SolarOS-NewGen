5b231f2a743c0b2d5feeac53a1412ea6d2896ef3
Nyx <nyxmalware@gmail.com>
2026-06-15 21:31:54 +0300

    Update fat32.c

--- src/fat32.c
+++ src/fat32.c
@@ -1,24 +1,23 @@
 #include "solarios.h"
-static uint32_t fat32_base_lba=0;
-static uint32_t fat32_bytes_per_sector=512;
-static uint32_t fat32_sectors_per_cluster=0;
-static uint32_t fat32_reserved_sectors=0;
-static uint32_t fat32_fat_size=0;
-static uint32_t fat32_root_cluster=0;
-static uint32_t fat32_data_start=0;
+static uint8_t ramdisk[1024*1024];
+static uint32_t ramdisk_size=0;
+static int ramdisk_ready=0;
 void fat32_init(void){
-fat32_base_lba=0;
-fat32_bytes_per_sector=512;
-fat32_sectors_per_cluster=0;
-fat32_reserved_sectors=0;
-fat32_fat_size=0;
-fat32_root_cluster=2;
-fat32_data_start=0;
+    for(int i=0;i<1024*1024;i++) ramdisk[i]=0;
+    const char* test_file="test.txt";
+    uint8_t* ptr=ramdisk;
+    for(int i=0;test_file[i];i++) ptr[i]=test_file[i];
+    ptr[8]=0;
+    ramdisk_size=1024;
+    ramdisk_ready=1;
 }
 int fat32_read_root_dir(void){
-return 0;
+    if(!ramdisk_ready) return -1;
+    return 0;
 }
 int fat32_open(const char *name,uint8_t *buf,uint32_t *size){
-(void)name;(void)buf;(void)size;
-return -1;
+    if(!ramdisk_ready||!name||!buf||!size) return -1;
+    for(uint32_t i=0;i<ramdisk_size&&i<*size;i++) buf[i]=ramdisk[i];
+    *size=ramdisk_size;
+    return 0;
 }
